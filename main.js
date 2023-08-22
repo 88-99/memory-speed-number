90a103442fbd6b41a434899346d9b0b79151832f
88-99 <xz835.525o39@yahoo.com>
2023-08-23 08:40:48 +0900

    桁数を25桁、制限時間を選択する形に変更。得点の表示、答え合わせで誤り箇所を反転表示する機能を追加。

--- main.js
+++ main.js
@@ -2,68 +2,70 @@
 
 import enquirer from "enquirer";
 
-const { Input } = enquirer;
 const { Select } = enquirer;
+const { Input } = enquirer;
 
-const prompt = new Input({
-  message: "How many digits for memorized number?",
+const prompt = new Select({
+  message: "How many seconds for limit time?",
+  choices: ["3", "5", "10", "30", "60", "120", "180"],
   hint: " (Enter a number.)",
 });
 
 prompt
   .run()
-  .then((answer) => {
-    return parseInt(answer);
+  .then((seconds) => {
+    const digits = 25;
+    const randomNumbers = generateRandomNumbers(digits);
+    const milliseconds = Number(seconds) * 1000;
+
+    return new Promise((resolve) => {
+      clearTerminalAfterDelay(digits, randomNumbers, milliseconds, () => {
+        resolve(randomNumbers);
+      });
+    });
   })
-  .then((digits) => {
-    const prompt = new Input({
-      message: "How many seconds for limit time?",
+  .then((randomNumbers) => {
+    const usersAnswer = new Input({
+      message: "Input your answer",
       hint: " (Enter a number.)",
+      multiline: true,
     });
 
-    prompt
-      .run()
-      .then((seconds) => {
-        const randomNumbers = generateRandomNumbers(digits);
-        const milliseconds = seconds * 1000;
+    usersAnswer.run().then((usersAnswer) => {
+      usersAnswer = usersAnswer.replace(/\s/g, "").split("").map(Number);
+      if (usersAnswer.join("") === randomNumbers.join("")) {
+        console.log("Perfect score. You are amazing!");
+      } else {
+        console.log(
+          `Your socre is ${calcPoints(
+            findIncorrectIndexes(randomNumbers, usersAnswer)
+          )}/100 points.`
+        );
+        console.log();
 
-        return new Promise((resolve) => {
-          clearTerminalAfterDelay(digits, randomNumbers, milliseconds, () => {
-            resolve(randomNumbers);
-          });
+        const prompt = new Select({
+          name: "memo",
+          message: "Do you check the answer?",
+          choices: ["yes", "no"],
         });
-      })
-      .then((randomNumbers) => {
-        const usersAnswer = new Input({
-          message: "Input your answer",
-          hint: " (Enter a number.)",
-          multiline: true,
-        });
-
-        usersAnswer.run().then((answer) => {
-          answer = answer.replace(/\s/g, "").split("").map(Number);
-          if (answer.join("") === randomNumbers.join("")) {
-            console.log("Correct");
-          } else {
-            console.log("Incorrect");
-
-            const prompt = new Select({
-              name: "memo",
-              message: "Do you check the answer?",
-              choices: ["yes", "no"],
-            });
-            prompt
-              .run()
-              .then((key) => {
-                if (key === "yes") {
-                  outputForm(randomNumbers);
-                }
-              })
-              .catch(console.error);
-          }
-        });
-      });
+        prompt
+          .run()
+          .then((key) => {
+            if (key === "yes") {
+              console.log("-- Your answer --");
+              printWithHighlight(
+                usersAnswer,
+                findIncorrectIndexes(randomNumbers, usersAnswer)
+              );
+              console.log("-- Correct answer --");
+              outputForm(randomNumbers);
+            }
+          })
+          .catch(console.error);
+      }
+    });
   });
+// });
 
 function generateRandomNumbers(digits) {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -80,9 +82,8 @@ function generateRandomNumbers(digits) {
 }
 
 function outputForm(randomNumbers) {
-  const chunkSize = 40;
+  const chunkSize = 5;
   const chunkedNumbers = [];
-
   const space = () => {
     return " ".repeat(12);
   };
@@ -106,11 +107,28 @@ function outputForm(randomNumbers) {
       console.log();
 
       if (i !== chunkedNumbers.length - 1) {
-        console.log(space() + "-".repeat(chunkedNumber.join(" ").length));
+        if (findHighlitedStrings(chunkedNumber) > 0) {
+          console.log(
+            space() +
+              "-".repeat(
+                chunkedNumber.join(" ").length -
+                  findHighlitedStrings(chunkedNumber) * 8
+              )
+          );
+        } else {
+          console.log(space() + "-".repeat(chunkedNumber.join(" ").length));
+        }
         console.log();
       }
     }
   }
+}
+
+function findHighlitedStrings(randomNumber) {
+  const highlitedStrings = randomNumber.filter(
+    (element) => typeof element === "string" && element.startsWith("\x1B[")
+  );
+  return highlitedStrings.length;
 }
 
 function clearTerminalAfterDelay(
@@ -119,19 +137,33 @@ function clearTerminalAfterDelay(
   milliseconds,
   callback
 ) {
-  // process.stdout.write(outputForm(randomNumbers));
   outputForm(randomNumbers);
 
-  let deleteLine = 3;
-  deleteLine = getDleteLine(digits, deleteLine);
-
   setTimeout(() => {
-    process.stdout.write(`\x1b[${deleteLine}A\x1b[0J`);
+    process.stdout.write("\x1b[18A\x1b[0J");
     callback();
   }, milliseconds);
 }
 
-function getDleteLine(digits, deleteLine) {
-  const step = Math.ceil(digits / 40) - 1;
-  return deleteLine + 4 * step;
+function findIncorrectIndexes(correctAnswer, usersAnswer) {
+  const incorrectIndexes = [];
+  for (let i = 0; i < correctAnswer.length; i++) {
+    if (correctAnswer[i] !== usersAnswer[i]) {
+      incorrectIndexes.push(i);
+    }
+  }
+
+  return incorrectIndexes;
+}
+
+function calcPoints(incorrectIndexes) {
+  return 100 - incorrectIndexes.length * 4;
+}
+
+function printWithHighlight(chars, highlightIndexes) {
+  const highlightedText = chars.map((char, index) =>
+    highlightIndexes.includes(index) ? `\x1b[7m${char}\x1b[0m` : char
+  );
+
+  return outputForm(highlightedText);
 }
