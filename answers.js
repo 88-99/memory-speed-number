28b0d892a81daf755afd08ef506b61a8e21f84f5
88-99 <xz835.525o39@yahoo.com>
2023-08-23 09:02:35 +0900

    正解とユーザの解答に関する処理を扱うAnswersクラスを作成

---
+++ answers.js
@@ -0,0 +1,60 @@
+"use strict";
+
+export class Answers {
+  #digits;
+  #correctAnswer;
+  #usersAnswer;
+
+  constructor(digits, usersAnswer) {
+    this.#digits = digits;
+    this.#correctAnswer = this.generateRandomNumbers();
+    this.#usersAnswer = usersAnswer;
+  }
+
+  get correctAnswer() {
+    return this.#correctAnswer;
+  }
+
+  set usersAnswer(inputValue) {
+    this.#usersAnswer = inputValue.replace(/\s/g, "").split("").map(Number);
+  }
+
+  generateRandomNumbers() {
+    const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    let randomNumbers = [];
+    for (let i = 0; i < this.#digits; i++) {
+      const randomIndex = Math.floor(Math.random() * numbers.length);
+      randomNumbers.push(numbers[randomIndex]);
+    }
+    return randomNumbers;
+  }
+
+  compareAnswers() {
+    return this.#correctAnswer.join("") === this.#usersAnswer.join("")
+      ? true
+      : false;
+  }
+
+  findIncorrectIndexes() {
+    const incorrectIndexes = [];
+    for (let i = 0; i < this.#correctAnswer.length; i++) {
+      if (this.#correctAnswer[i] !== this.#usersAnswer[i]) {
+        incorrectIndexes.push(i);
+      }
+    }
+    return incorrectIndexes;
+  }
+
+  calcPoints() {
+    return 100 - this.findIncorrectIndexes().length * 4;
+  }
+
+  generateHighlightedNumbers() {
+    const highlightedNumbers = this.#usersAnswer.map((number, index) =>
+      this.findIncorrectIndexes().includes(index)
+        ? `\x1b[7m${number}\x1b[0m`
+        : number,
+    );
+    return highlightedNumbers;
+  }
+}
--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 "use strict";
 
 import { Formatter } from "./formatter.js";
+import { Answers } from "./answers.js";
 import enquirer from "enquirer";
 
 const { Select, Input } = enquirer;
@@ -34,24 +35,23 @@ async function main() {
     });
   }
 
-  function announcementResult(correctAnswer, usersAnswer) {
+  function announcementResult(digits, answers) {
     return new Promise((resolve) => {
-      usersAnswer = usersAnswer.replace(/\s/g, "").split("").map(Number);
-      if (usersAnswer.join("") === correctAnswer.join("")) {
+      if (answers.compareAnswers()) {
         console.log("Perfect score. You are amazing!");
       } else {
         console.log(
-          `Your socre is ${calcPoints(
-            findIncorrectIndexes(correctAnswer, usersAnswer)
+          `Your socre is ${answers.calcPoints(
+            answers.findIncorrectIndexes
           )}/100 points.`
         );
         console.log();
-        resolve(usersAnswer);
+        resolve();
       }
     });
   }
 
-  function checkAnswerOrNot(correctAnswer, usersAnswer) {
+  function checkAnswerOrNot(answers) {
     const checkAnswerOrNotPrompt = new Select({
       message: "Do you check the answer?",
       choices: ["yes", "no"],
@@ -61,45 +61,33 @@ async function main() {
         if (key === "yes") {
           console.log("-- Your answer --");
           const formattedUsersAnswer = new Formatter(
-            generateHighlightedNumbers(
-              usersAnswer,
-              findIncorrectIndexes(correctAnswer, usersAnswer)
-            )
+            answers.generateHighlightedNumbers()
           );
           formattedUsersAnswer.selectFormat();
+
           console.log("-- Correct answer --");
-          const formattedCorrectAnswer = new Formatter(correctAnswer);
+          const formattedCorrectAnswer = new Formatter(answers.correctAnswer);
           formattedCorrectAnswer.selectFormat();
         }
       });
   }
 
   try {
-    const seconds = await selectSecondsPrompt.run();
     const digits = 25;
-    const correctAnswer = generateRandomNumbers(digits);
-    await asyncClearTerminalAfterDelay(digits, seconds, correctAnswer);
+    const seconds = await selectSecondsPrompt.run();
+    const answers = new Answers(digits, "");
+    await asyncClearTerminalAfterDelay(digits, seconds, answers.correctAnswer);
 
     let usersAnswer = await inputUsersAnswerPrompt.run();
-    usersAnswer = await announcementResult(correctAnswer, usersAnswer);
+    answers.usersAnswer = usersAnswer;
+    await announcementResult(digits, answers);
 
-    checkAnswerOrNot(correctAnswer, usersAnswer);
+    checkAnswerOrNot(answers);
   } catch (error) {
     console.error("Error:", error);
   }
 }
 main();
-
-function generateRandomNumbers(digits) {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  let randomNumbers = [];
-
-  for (let i = 0; i < digits; i++) {
-    const randomIndex = Math.floor(Math.random() * numbers.length);
-    randomNumbers.push(numbers[randomIndex]);
-  }
-  return randomNumbers;
-}
 
 function clearTerminalAfterDelay(digits, seconds, correctAnswer, callback) {
   const formatter = new Formatter(correctAnswer);
@@ -109,25 +97,4 @@ function clearTerminalAfterDelay(digits, seconds, correctAnswer, callback) {
     process.stdout.write("\x1b[18A\x1b[0J");
     callback();
   }, seconds * 1000);
-}
-
-function findIncorrectIndexes(correctAnswer, usersAnswer) {
-  const incorrectIndexes = [];
-  for (let i = 0; i < correctAnswer.length; i++) {
-    if (correctAnswer[i] !== usersAnswer[i]) {
-      incorrectIndexes.push(i);
-    }
-  }
-  return incorrectIndexes;
-}
-
-function calcPoints(incorrectIndexes) {
-  return 100 - incorrectIndexes.length * 4;
-}
-
-function generateHighlightedNumbers(numbers, highlightIndexes) {
-  const highlightedNumbers = numbers.map((number, index) =>
-    highlightIndexes.includes(index) ? `\x1b[7m${number}\x1b[0m` : number
-  );
-  return highlightedNumbers;
 }
